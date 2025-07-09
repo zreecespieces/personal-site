baf10f5b334bb30b747662c5680b895add535202
Zachary Reece <zachdreece@gmail.com>
2025-07-09 17:52:13 -0600

    updated festivals with roo and deadrocks

--- src/app/music-festivals/festivals.ts
+++ src/app/music-festivals/festivals.ts
@@ -1,130 +1,149 @@
 export interface Festival {
-    name: string;
-    year: string;
-    location: string;
-    url?: string;
-    image: string;
-    description?: string;
+  name: string;
+  year: string;
+  location: string;
+  url?: string;
+  image: string;
+  description?: string;
 }
 
 export const festivals: Festival[] = [
-    {
-        name: "Bonnaroo",
-        year: "2017",
-        location: "Manchester, Tennessee",
-        url: "https://bonnaroo.com/",
-        image: "/bonnaroo.png",
-        description: ""
-    },
-    {
-        name: "Decadence",
-        year: "2018",
-        location: "Denver, Colorado",
-        url: "https://decadencenye.com",
-        image: "/decadence.png",
-        description: ""
-    },
-    {
-        name: "Governor's Ball",
-        year: "2018",
-        location: "New York, New York",
-        url: "https://www.governorsballmusicfestival.com/",
-        image: "/gov-ball.png",
-        description: ""
-    },
-    {
-        name: "Bonnaroo",
-        year: "2019",
-        location: "Manchester, Tennessee",
-        url: "https://bonnaroo.com/",
-        image: "/bonnaroo.png",
-        description: ""
-    },
-    {
-        name: "Austin City Limits",
-        year: "2019",
-        location: "Austin, Texas",
-        url: "https://www.aclfestival.com/",
-        image: "/acl.png",
-        description: ""
-    },
-    {
-        name: "Lollapalooza",
-        year: "2021",
-        location: "Chicago, Illinois",
-        url: "https://lollapalooza.com/",
-        image: "/lolla.png",
-        description: ""
-    },
-    {
-        name: "Fake Bonnaroo",
-        year: "2021",
-        location: "Short Mountain Distillery",
-        image: "/fake-bonnaroo.png",
-        description: "When Bonnaroo was cancelled in 2021 due to Hurricane Ida, some locals threw a popup festival about two hours away from where it would have been and a bunch of us went to make the best out of what would have been a sad weekend."
-    },
-    {
-        name: "Dancefestopia",
-        year: "2021",
-        location: "Kansas City, Missouri",
-        image: "/dancefest.png",
-        description: "",
-        url: "https://www.dancefestopia.com/"
-    },
-    {
-        name: "Lights All Night",
-        year: "2022",
-        location: "Dallas, Texas",
-        image: "/lan.png",
-        description: "",
-        url: "https://lightsallnight.com/"
-    },
-    {
-        name: "Bonnaroo",
-        year: "2022",
-        location: "Manchester, Tennessee",
-        url: "https://bonnaroo.com/",
-        image: "/bonnaroo.png",
-        description: ""
-    },
-    {
-        name: "Breakaway",
-        year: "2022",
-        location: "Kansas City, Missouri",
-        image: "/breakaway.png",
-        description: "",
-        url: "https://www.breakawayfestival.com/"
-    },
-    {
-        name: "Bonnaroo",
-        year: "2023",
-        location: "Manchester, Tennessee",
-        url: "https://bonnaroo.com/",
-        image: "/bonnaroo.png",
-        description: ""
-    },
-    {
-        name: "Bonnaroo",
-        year: "2024",
-        location: "Manchester, Tennessee",
-        url: "https://bonnaroo.com/",
-        image: "/bonnaroo.png",
-        description: ""
-    },
-    {
-        name: "Hulaween",
-        year: "2024",
-        location: "Live Oak, Florida",
-        image: "/hulaween.png",
-        description: "",
-        url: "https://hulaween.com",
-    },
-    {
-        name: "Decadence",
-        year: "2025",
-        location: "Denver, Colorado",
-        image: "/decadence.png",
-        description: "",
-        url: "https://decadencenye.com",
-    }
-]
+  {
+    name: "Bonnaroo",
+    year: "2017",
+    location: "Manchester, Tennessee",
+    url: "https://bonnaroo.com/",
+    image: "/bonnaroo.png",
+    description: "",
+  },
+  {
+    name: "Decadence",
+    year: "2018",
+    location: "Denver, Colorado",
+    url: "https://decadencenye.com",
+    image: "/decadence.png",
+    description: "",
+  },
+  {
+    name: "Governor's Ball",
+    year: "2018",
+    location: "New York, New York",
+    url: "https://www.governorsballmusicfestival.com/",
+    image: "/gov-ball.png",
+    description: "",
+  },
+  {
+    name: "Bonnaroo",
+    year: "2019",
+    location: "Manchester, Tennessee",
+    url: "https://bonnaroo.com/",
+    image: "/bonnaroo.png",
+    description: "",
+  },
+  {
+    name: "Austin City Limits",
+    year: "2019",
+    location: "Austin, Texas",
+    url: "https://www.aclfestival.com/",
+    image: "/acl.png",
+    description: "",
+  },
+  {
+    name: "Lollapalooza",
+    year: "2021",
+    location: "Chicago, Illinois",
+    url: "https://lollapalooza.com/",
+    image: "/lolla.png",
+    description: "",
+  },
+  {
+    name: "Fake Bonnaroo",
+    year: "2021",
+    location: "Short Mountain Distillery",
+    image: "/fake-bonnaroo.png",
+    description:
+      "When Bonnaroo was cancelled in 2021 due to Hurricane Ida, some locals threw a popup festival about two hours away from where it would have been and a bunch of us went to make the best out of what would have been a sad weekend.",
+  },
+  {
+    name: "Dancefestopia",
+    year: "2021",
+    location: "Kansas City, Missouri",
+    image: "/dancefest.png",
+    description: "",
+    url: "https://www.dancefestopia.com/",
+  },
+  {
+    name: "Lights All Night",
+    year: "2022",
+    location: "Dallas, Texas",
+    image: "/lan.png",
+    description: "",
+    url: "https://lightsallnight.com/",
+  },
+  {
+    name: "Bonnaroo",
+    year: "2022",
+    location: "Manchester, Tennessee",
+    url: "https://bonnaroo.com/",
+    image: "/bonnaroo.png",
+    description: "",
+  },
+  {
+    name: "Breakaway",
+    year: "2022",
+    location: "Kansas City, Missouri",
+    image: "/breakaway.png",
+    description: "",
+    url: "https://www.breakawayfestival.com/",
+  },
+  {
+    name: "Bonnaroo",
+    year: "2023",
+    location: "Manchester, Tennessee",
+    url: "https://bonnaroo.com/",
+    image: "/bonnaroo.png",
+    description: "",
+  },
+  {
+    name: "Bonnaroo",
+    year: "2024",
+    location: "Manchester, Tennessee",
+    url: "https://bonnaroo.com/",
+    image: "/bonnaroo.png",
+    description: "",
+  },
+  {
+    name: "Hulaween",
+    year: "2024",
+    location: "Live Oak, Florida",
+    image: "/hulaween.png",
+    description: "",
+    url: "https://hulaween.com",
+  },
+  {
+    name: "Decadence",
+    year: "2025",
+    location: "Denver, Colorado",
+    image: "/decadence.png",
+    description: "",
+    url: "https://decadencenye.com",
+  },
+  {
+    name: "Bonnaroo",
+    year: "2025",
+    location: "Manchester, Tennessee",
+    url: "https://bonnaroo.com/",
+    image: "/cancelled-bonnaroo.png",
+    description:
+      "I was at the infamous Bonnaroo in 2025 where it was cancelled after a single day of heavy rain, only one day into the start of the festival. Luckily I had early entry on Tuesday so at least I got a couple days of camping, and met some great new friends. We all pitched in on an Airbnb in Nashville and still made the best out of the weekend - for my second time after the cancellation in 2021.",
+  },
+  {
+    name: "DeadRocks XI",
+    year: "2025",
+    location: "Denver, Colorado",
+    image: "/deadrocks-2025.png",
+    description:
+      "My first DeadRocks - the annual event put on by Zeds Dead, currently my favorite EDM artist. Their latest album is probably my top EDM album of all time and it was an incredibly experience getting to see it live with such a big production. I went to both nights at Red Rocks along with the hangover set at Mission Ballroom.",
+    url: "https://deadbeatsofficial.com/deadrocks",
+  },
+];
